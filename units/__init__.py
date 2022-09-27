a116c3369f6430355466fc0cd0d9c5f2c78ea196
Bjørn Erik Juel <bjorneju@gmail.com>
2022-09-27 21:47:25 +0200

    Update __init__.py

--- units/__init__.py
+++ units/__init__.py
@@ -1,13 +1,1 @@
 
-import pyphi
-
-import string
-
-import numpy as np
-import pandas as pd
-import networkx as nx
-import matplotlib.pyplot as plt
-from tqdm import tqdm
-
-from unit_functions import *
-from network_builder import *
