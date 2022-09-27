d7498fd302c12d75644423a480c61a039b4df8d2
Bjørn Erik Juel <bjorneju@gmail.com>
2022-09-27 21:20:40 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author="Bjørn E juel",
     author_email="bjorneju@gmail.com",
     license="BSD 2-clause",
-    packages=["units"],
+    packages=[],
     install_requires=[
         "tqdm",
         "numpy",
