49f34253e4480ed2d00b44d7a9f848287e1b87a2
Bjørn Erik Juel <bjorneju@gmail.com>
2022-09-27 17:50:09 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name="pyphi_units",
+    version="0.1.0",
+    description="Create pyphi substrates from the ground up",
+    url="https://github.com/bjorneju/pyphi_units",
+    author="Bjørn E juel",
+    author_email="bjorneju@gmail.com",
+    license="BSD 2-clause",
+    packages=["pyphi_units"],
+    install_requires=[
+        "tqdm",
+        "numpy",
+        "pandas",
+        "networkx",
+        "pyphi",
+        "matplotlib",
+        "string",
+    ],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
